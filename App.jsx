c228fd62b0d6ed4ae4cff67b01b2b40ef630a07a
Richzaldi <88223003+Ryzaldi@users.noreply.github.com>
2025-08-09 10:09:07 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { Button, Title3 } from "@fluentui/react-components";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Title3>🚀 Fluent UI + Codespaces</Title3>
+      <Button appearance="primary">Click Me</Button>
+    </div>
+  );
+}
